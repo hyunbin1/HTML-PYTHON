14724fe8eb11afc05c49ab212e0ae26cb1e9005e
hyunbin1 <kimgusqls1@gmail.com>
2020-07-19 11:05:42 +0900

    django 시작하는법

---
+++ django.py
@@ -0,0 +1,20 @@
+
+#! django start
+
+#? 부트스트랩 사용 = 오프소스 프레임 워크
+# 원하는 파일을 html에 복붙한 후에 부트스트랩 CDN을 html 헤더 부분에 넣어준다.
+
+#? djangoi 시작하는 과정
+
+# .gitignore 폴더 생성해주기 -> git ignore.io에 들어가서 python,django, visual studio code를 첨가해준다
+# 모두 복붙한다음에 14번째줄 media 아래 - venv라고 추가해준다
+
+
+
+
+
+
+
+
+
+
